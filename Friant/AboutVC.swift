c3da0ccfe7259712637d55dceace6067e2275c89
Nikka-Sher <npiterman@gmail.com>
2017-06-26 04:22:11 -0700

    about VC segue back btn

--- Friant/AboutVC.swift
+++ Friant/AboutVC.swift
@@ -16,20 +16,11 @@ class AboutVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func backBtnPressed(_ sender: Any) {
+        
+             dismiss(animated: true, completion: nil)
+        
+        //animation selection occurrs here
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
